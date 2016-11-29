cb67fa87b71d6b8b77fd0f5d06658da55a768cac
thiesgehrmann <thiesgehrmann@gmail.com>
2016-11-29 13:27:59 +0100

    Figured out the mechanism

--- asperaconnect.rb
+++ asperaconnect.rb
@@ -1,8 +1,4 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-class Asperaconnect < Formula
+	class Asperaconnect < Formula
   desc "Aspera connect package"
   homepage "http://asperasoft.com//"
   url "http://download.asperasoft.com/download/sw/connect/3.6.2/aspera-connect-3.6.2.117442-linux-64.tar.gz"
@@ -11,34 +7,9 @@ class Asperaconnect < Formula
   sha256 "4f69f2cd410ee770dbd81002af7be79715a6de77cbb84fc1bf12ad7a2751a457"
 
   def install
-    #tarball = "aspera-connect-3.6.2.117442-linux-64.tar.gz"
     skip = `awk '/^__ARCHIVE_FOLLOWS__/ { print NR + 1; exit 0; }' #{@@tarball}.sh`.chomp
     system "tail -n +#{skip} #{@@tarball}.sh | tar xzpo -C ./ -f -"
-
-    bin.install "bin/asperaconnect"
-    bin.install "bin/asperaconnect.bin"
-    bin.install "bin/ascp"
-    bin.install "bin/asperacrypt"
-    lib.install "lib/libpng12.so.0"
-    lib.install "lib/libaudio.so.2"
-    lib.install "lib/libgcc_s.so.1"
-    lib.install "lib/libstdc++.so.6"
-    lib.install "lib/libQtSql.so.4"
-    lib.install "lib/libQtXml.so.4"
-    lib.install "lib/libQtNetwork.so.4"
-    lib.install "lib/libQtGui.so.4"
-    lib.install "lib/libQtCore.so.4"
-    lib.install "lib/libnpasperaweb_3.6.2.117442.so"
-    etc.install "etc/localhost.key"
-    etc.install "etc/localhost.crt"
-    etc.install "etc/asperaconnect.path"
-    etc.install "etc/asperaweb_id_dsa.putty"
-    etc.install "etc/asperaweb_id_dsa.openssh"
-    etc.install "etc/aspera_web_key.pem"
-    etc.install "etc/aspera_web_cert.pem"
-    etc.install "etc/aspera-license"
-    etc.install "etc/aspera.conf"
-
+    system "cp -r bin lib etc #{prefix}"
   end
 
   test do
--- canu-git.rb
+++ canu-git.rb
@@ -1,7 +1,3 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class CanuGit < Formula
   desc "Canu Assembler"
   homepage "https://github.com/marbl/canu"
@@ -9,18 +5,7 @@ class CanuGit < Formula
   version "999"
   sha256 "9b7fdc074593969a00a6ab3cd5c3d70d5348f9c25d194d08856a486ee097380b"
 
-  # depends_on "cmake" => :build
-  # depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
     Dir.chdir("src")
     system "make -j 12"
     system "find ../ > /tmp/loc"
@@ -30,15 +15,6 @@ class CanuGit < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test canu`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
